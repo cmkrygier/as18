ea00a1b11646b9726bc4840ea8962b553b2b53d2
cmkrygier <36484191+cmkrygier@users.noreply.github.com>
2021-04-09 15:27:31 -0400

    started the html front end

---
+++ as19.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+        </tr>
+    </thead>
+    <tbody>
+ 
+    <?php
+
+        echo "<tr>";
+        #for each loop here to create the tags
+        echo "<td>Mark</td>";
+        echo "<td>Otto</td>";
+        echo "<td>@mdo</td>";
+        echo "</tr>";
+        
+        ?>
+
+    </tbody>
+</table>
+
+</html>
